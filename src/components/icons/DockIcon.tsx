178e96b066ece7370993d75e8448e8e96186174a
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-08-15 14:31:11 +0600

    dock icon

---
+++ src/components/icons/DockIcon.tsx
@@ -0,0 +1,21 @@
+// @ts-ignore
+export default function DockIcon(props) {
+    return (
+        <svg
+            {...props}
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+        >
+            <path d="M2 8h20" />
+            <rect width="20" height="16" x="2" y="4" rx="2" />
+            <path d="M6 16h12" />
+        </svg>
+    )
+}
