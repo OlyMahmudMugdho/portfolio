c8cc7706b68d3eeb04c85b6a132208383ae0b5af
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-08-18 01:29:03 +0600

    add resume

--- src/components/custom/Hero.tsx
+++ src/components/custom/Hero.tsx
@@ -9,18 +9,19 @@ export default function Hero() {
                 <p className="text-lg md:text-xl">Full-Stack Web Developer & DevOps Engineer</p>
                 <div className="flex flex-col gap-4 sm:flex-row">
                     <Link
-                        href="#projects"
-                        className="inline-flex h-10 items-center justify-center rounded-md bg-primary-foreground px-6 text-sm font-medium text-primary shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
+                        href="https://olymahmud.tiiny.site/"
+                        target={"_blank"}
+                        className="inline-flex h-10 items-center justify-center rounded-md bg-primary-foreground px-6 text-sm font-medium text-primary shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
                         prefetch={false}
                     >
-                        View Projects
+                        Download Resume
                     </Link>
                     <Link
-                        href="#contacts"
+                        href="#projects"
                         className="inline-flex h-10 items-center justify-center rounded-md border border-primary-foreground bg-primary px-6 text-sm font-medium text-primary-foreground shadow-sm transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
                         prefetch={false}
                     >
-                        Contact Me
+                        View Projects
                     </Link>
                 </div>
             </div>
