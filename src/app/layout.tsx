6c7056898a2dce6f9f9e9cc28d7f78e94254a2c0
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-09-07 22:02:08 +0600

    updated head section and meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,26 +3,28 @@
 import { IBM_Plex_Mono } from 'next/font/google'
 import { cn } from '@/lib/utils'
 import './globals.css'
+import Head from '@/components/custom/heading-sections/Head'
 
 const fontHeading = IBM_Plex_Mono({
-    subsets: ['latin'],
-    display: 'swap',
-    variable: '--font-heading',
-    weight: '100'
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-heading',
+  weight: '100'
 })
 
 const fontBody = IBM_Plex_Mono({
-    subsets: ['latin'],
-    display: 'swap',
-    variable: '--font-body',
-    weight: '100'
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-body',
+  weight: '100'
 })
 
 // @ts-ignore
 export default function Layout({ children }) {
   return (
     <html lang="en">
-      <body 
+      <Head />
+      <body
         className={cn(
           'antialiased',
           fontHeading.variable,
