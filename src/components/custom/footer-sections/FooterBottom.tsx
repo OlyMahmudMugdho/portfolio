e55a540e4fb8daedd94c7c84a07cc3ef9d46f10f
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-09-07 21:32:03 +0600

    footer bottom section

---
+++ src/components/custom/footer-sections/FooterBottom.tsx
@@ -0,0 +1,16 @@
+
+
+function FooterBottom() {
+    return (
+        <div className="border-t border-gray-300 mt-8 pt-6 flex flex-col justify-center items-center">
+            <p className="text-sm text-gray-300 text-center">
+                &copy; 2024 M. Oly Mahmud. All rights reserved.
+            </p>
+            <p className="text-sm text-gray-300 text-center mt-4">
+                Developed by M. Oly Mahmud
+            </p>
+        </div>
+    )
+}
+
+export default FooterBottom
