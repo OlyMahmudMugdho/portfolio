edf284ac17f891c5222b0705e7a89b64c7f882f7
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2026-01-09 21:12:01 +0600

    add the updated logo

--- components/sections/newsletter.tsx
+++ components/sections/newsletter.tsx
@@ -24,7 +24,7 @@ export function NewsletterSection() {
                             {/* Newsletter Logo */}
                             <div className="shrink-0 flex items-center justify-center">
                                 <Image
-                                    src="https://res.cloudinary.com/dnmubeloc/image/upload/v1767969976/others/software-system-design-newsletter-logo-removebg-preview_giwfz6.png"
+                                    src="https://res.cloudinary.com/dnmubeloc/image/upload/v1767971440/others/Gemini_Generated_Image_91lzgh91lzgh91lz-removebg-preview_gf1jlv.png"
                                     alt="Software Systems Engineering Logo"
                                     width={160}
                                     height={160}
