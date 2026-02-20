bc5ef60e81bba8a7ac44b35f64e629a2a0dfa04d
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2026-02-20 21:23:16 +0600

    consolata font for blog

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -1,5 +1,8 @@
 import { Navbar } from "@/components/navbar"
 import { Footer } from "@/components/sections/footer"
+import { Inconsolata } from 'next/font/google'
+
+const inconsolata = Inconsolata({ subsets: ['latin'] })
 
 export default function BlogLayout({
     children,
@@ -9,7 +12,7 @@ export default function BlogLayout({
     return (
         <div className="flex min-h-screen flex-col bg-background">
             <Navbar />
-            <main className="flex-1 pt-24 pb-12">
+            <main className={`flex-1 pt-24 pb-12 ${inconsolata.className}`}>
                 {children}
             </main>
             <Footer />
